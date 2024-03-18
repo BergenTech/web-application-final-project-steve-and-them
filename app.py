d64b13009c4b494c01d72d9a9b6b086397aebdd5
SteveChoi1219 <stevechoi268@gmail.com>
2024-03-18 10:13:52 -0400

    Register routes

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        # Extract form data
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        role = request.form.get('role')
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
