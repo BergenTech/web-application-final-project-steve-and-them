7921b29f8a4f41ac413cb1a6c35265ee044833ce
SteveChoi1219 <stevechoi268@gmail.com>
2024-06-04 08:32:25 -0400

    wsgi

--- app.py
+++ app.py
@@ -409,7 +409,7 @@ def received_messages(user_id):
 def slides():
     return render_template('slides.html')
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
